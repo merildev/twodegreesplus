6d77ae6ad38be4999a0932a2d6064aa2daa7bdae
Meril Pilon <meril@blockchain.com>
2018-09-25 20:21:04 +0100

    chore(tools): add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,37 @@
-const path = require('path');
-const outputDir = path.join(__dirname, "build/");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const outputDir = path.join(__dirname, "build/")
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production'
+
+const PATHS = {
+  build: `${__dirname}/build`,
+  src: `${__dirname}/src`,
+}
+
+const devPlugins = [
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: `${PATHS.src}/index.html`,
+  }),
+]
 
 module.exports = {
-  entry: './src/Index.bs.js',
+  devServer: {
+    contentBase: PATHS.build,
+    host: 'localhost',
+  },
+  entry: {
+    app: [
+      'webpack-dev-server/client?http://localhost:8080',
+      './src/Index.bs.js',
+    ],
+  },
   mode: isProd ? 'production' : 'development',
   output: {
-    path: outputDir,
-    publicPath: outputDir,
     filename: 'Index.js',
+    path: PATHS.build,
+    publicPath: '/',
   },
-};
+  plugins: devPlugins,
+}
