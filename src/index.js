773e919e5b8f029ecb50e4e28bb585e3bfe55aad
Meril Pilon <meril@blockchain.com>
2018-09-25 21:09:33 +0100

    chore(deps): use create-react-app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/app'
+
+const rootNode = document.getElementById('root')
+
+ReactDOM.render(<App />, rootNode)
