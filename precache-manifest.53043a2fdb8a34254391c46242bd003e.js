e40309407fe4d0c484cc3542260fdbf32aaa7c62
Meril Pilon <meril@blockchain.com>
2018-10-07 22:26:27 +0100

    Updates

---
+++ precache-manifest.53043a2fdb8a34254391c46242bd003e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6061c4069fa96fae483",
+    "url": "/twodegreesplus/static/js/runtime~main.e6061c40.js"
+  },
+  {
+    "revision": "a707e0842661568803d7",
+    "url": "/twodegreesplus/static/js/main.a707e084.chunk.js"
+  },
+  {
+    "revision": "d81e662e4d9a8359d0cf",
+    "url": "/twodegreesplus/static/js/1.d81e662e.chunk.js"
+  },
+  {
+    "revision": "62765c737fe6cd9e0fab51dbff20d1ec",
+    "url": "/twodegreesplus/index.html"
+  }
+];
